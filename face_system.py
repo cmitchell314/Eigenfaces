9cba6348b46275459ad22ab0ab0879a707dc60b6
Connor Mitchell <43866950+cmitchell314@users.noreply.github.com>
2023-11-21 19:29:48 -0500

    testing updated - additional imgs

--- face_system.py
+++ face_system.py
@@ -23,6 +23,7 @@ class App:
         # INSTANCE STATE VARIABLES
         self.user_id = None # name of current user 
         self.log_next_img = False
+        self.analyze_next_img = False
 
         # INITIALIZATION of algorithms
         initialize()
@@ -30,7 +31,7 @@ class App:
         # SETUP of GUI
         self.root = root
         self.root.title('Demo Facial Recognition System')
-        width = 1000
+        width = 1150
         height = (192 * 2) + 240
         self.root.geometry(f'{width}x{height}')
 
@@ -38,8 +39,11 @@ class App:
 
         # top frame - banner, title, version, etc.
         top_frame = tk.Frame(self.root, width=width, height=100)
-        top_frame.configure(bg='black')
+        top_frame.configure(bg='gray')
         top_frame.pack(side='top')
+
+        banner = tk.Label(top_frame, text='Facial Recognition System', font=('Calibri', 40), bg='black', fg='white')
+        banner.pack(side='top', padx=10, pady=10)
 
         # bottom frame - current id, save image button, etc.
         bottom_frame = tk.Frame(self.root, width=width, height=100)
@@ -48,28 +52,52 @@ class App:
 
         # center frame - left, center, right panels
         center_frame = tk.Frame(self.root, width=width, height=root.winfo_height() - 200)
-        center_frame.configure(highlightbackground='blue', highlightthickness=3)
+        center_frame.configure(highlightbackground='blue', highlightthickness=0)
 
         # left panel - see existing saved faces
         left_panel = tk.Frame(center_frame, width=300, height=height - 200)
-        left_panel.configure(highlightbackground='red', highlightthickness=3)
+        left_panel.configure(highlightbackground='gray', highlightthickness=3)
         left_panel.pack(side='left')
+
+        # left panel instructions 
+        self.left_panel_text = tk.Label(left_panel, text='Center face to take picture', font=('Calibri', 20))
+        self.left_panel_text.pack(side='top', padx=10, pady=10)
+        self.left_panel_text1 = tk.Label(left_panel, text='Enter user id in box', font=('Calibri', 20))
+        self.left_panel_text1.pack(side='top', padx=10, pady=10)
+        self.left_panel_text2 = tk.Label(left_panel, text='Press "Save Image" to save', font=('Calibri', 20))
+        self.left_panel_text2.pack(side='top', padx=10, pady=10)
+        self.left_panel_text3 = tk.Label(left_panel, text='Press "Match" to analyze', font=('Calibri', 20))
+        self.left_panel_text3.pack(side='top', padx=10, pady=10)
+        self.left_panel_text4 = tk.Label(left_panel, text='See match on right panel', font=('Calibri', 20))
+        self.left_panel_text4.pack(side='top', padx=10, pady=10)
+        self.left_panel_text5 = tk.Label(left_panel, text='Press "Reset All" to delete ALL data', font=('Calibri', 20))
+        self.left_panel_text5.pack(side='top', padx=10, pady=10)
 
         # center panel - see current image
         self.center_panel = tk.Frame(center_frame, width=width - 500, height=height - 200)
         self.center_panel.pack(side='left')
-        self.center_panel.configure(highlightbackground='gray', highlightthickness=10)
+        self.center_panel.configure(bg='gray', highlightbackground='gray', highlightthickness=10)
 
         # right panel - closest match, guessed name, confidence (?), etc.
-        right_panel = tk.Frame(center_frame, width=200, height=height - 200)
-        right_panel.configure(highlightbackground='red', highlightthickness=3)
-        right_panel.pack(side='left')
+        self.right_panel = tk.Frame(center_frame, width=200, height=height - 200)
+        self.right_panel.configure(highlightbackground='gray', highlightthickness=3)
+        self.right_panel.pack(side='left')
 
         center_frame.pack(side='top')
 
         # img feed
         self.img_can = tk.Canvas(self.center_panel, width=168*2, height=192 * 2)
         self.img_can.pack(anchor=tk.CENTER)
+
+        # id photo
+        self.recog_text = tk.Label(self.right_panel, text='Best Match', font=('Calibri', 20))
+        self.recog_text.pack(side='top', padx=10, pady=10)
+
+        self.recog_id = tk.Label(self.right_panel, text='---', font=('Calibri', 15))
+        self.recog_id.pack(side='top', padx=10, pady=10)
+        
+        self.id_can = tk.Canvas(self.right_panel, width=168, height=192)
+        self.id_can.pack(side='top', padx=10, pady=10)
 
         # bottom frame contents
         self.id_tag = tk.Label(bottom_frame, text='Current User ID: ', font=('Calibri', 30))
@@ -81,6 +109,8 @@ class App:
         # self.save_img_button = tk.Button(bottom_frame, text='Save Image', font=('Calibri', 20), command=(lambda: self.log_next_img=True))
         self.clear_all_button = tk.Button(bottom_frame, text='Reset All', font=('Calibri', 20), command= erase_all_user_faces)
         self.clear_all_button.pack(side='right', padx=10, pady=10)
+        self.analyze_button = tk.Button(bottom_frame, text='Match', font=('Calibri', 20), command= self.set_analyze_next)
+        self.analyze_button.pack(side='right', padx=10, pady=10)
         self.save_img_button = tk.Button(bottom_frame, text='Save Image', font=('Calibri', 20), command= self.set_grab_next)
         self.save_img_button.pack(side='right', padx=10, pady=10)
 
@@ -88,6 +118,9 @@ class App:
 
     def set_grab_next(self):
         self.log_next_img = True
+
+    def set_analyze_next(self):
+        self.analyze_next_img = True
 
     def update(self):
         ### Update camera panel
@@ -110,6 +143,16 @@ class App:
                 # plt.imshow(g_frm, cmap='gray')
                 # plt.show()
             self.log_next_img = False
+
+        if (self.analyze_next_img):
+            if ret:
+                g_frm = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)[::2, ::2]
+                id_nm, id_vec, _ = get_closest_face(g_frm)
+                self.recog_id.configure(text=id_nm)
+                id_photo = ImageTk.PhotoImage(Image.fromarray(np.flip(id_vec.reshape(192, 168), axis=1)))
+                self.id_can.create_image(0, 0, image=id_photo, anchor=tk.NW)
+                self.id_can.image = id_photo
+            self.analyze_next_img = False
 
 
         # schedule next update
@@ -157,17 +200,23 @@ def initialize():
 def get_closest_face(img):
     global e_faces, mean_face, user_faces, user_labels
 
+    print('Analyzing image')
     if img.shape[0] != 192 or img.shape[1] != 168:
         return None
     
     face_eform = np.matmul(e_faces.T, img.flatten() - mean_face)
 
     # find closest face - use KNN
+    faces = [i for i in range(user_faces.shape[0])]
+    eucl_dst = {}
+    for i in faces:
+        eucl_dst[i] = np.linalg.norm(face_eform - user_faces[i])
+    faces.sort(key=lambda x: eucl_dst[x])
+    print(faces)
+    print([eucl_dst[i] for i in faces])
+    u_fac = np.matmul(e_faces, user_faces[faces[0]]) + mean_face
+    return (user_labels[faces[0]], u_fac, eucl_dst[faces[0]])
 
-    s
-
-    
-    return None
 
 def add_face(img, user_name):
     global e_faces, mean_face, user_faces, user_labels
@@ -175,9 +224,9 @@ def add_face(img, user_name):
     if img.shape[0] == 192 and img.shape[1] == 168 and user_name != '':
         face_eform = np.matmul(e_faces.T, img.flatten() - mean_face)
         if user_faces.shape[0] == 0:
-            user_faces = np.array([img.flatten()])
+            user_faces = np.array([face_eform])
         else:
-            user_faces = np.append(user_faces, [img.flatten()], axis=0)
+            user_faces = np.append(user_faces, [face_eform], axis=0)
         np.save('user_faces_app.npy', user_faces)
 
         if user_name in user_labels:
